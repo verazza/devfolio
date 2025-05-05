873501913b7c3b20682eb3b072d86033f95bed3f
bella <maekawatakaya10.8@gmail.com>
2025-05-05 19:33:02 +0900

    change class of links in contact.tsx

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -11,9 +11,9 @@ export default createRoute(async (c) => {
           <p class="mt-2 text-gray-600">連絡や問い合わせは以下から。</p>
         </header>
         <section class="mt-8 flex flex-col items-center">
-          <a href={profile.social.x} target="_blank" rel="noopener noreferrer">X-@verazza_</a>
+          <a href={profile.social.x} target="_blank" rel="noopener noreferrer" class="text-gray-300 hover:text-white transition duration-200">X-@verazza_</a>
           <span class="mt-2">または</span>
-          <a href={`mailto:${profile.contact.email}`} class="mt-2">{profile.contact.email}</a>
+          <a href={`mailto:${profile.contact.email}`} class="mt-2 text-gray-300 hover:text-white transition duration-200">{profile.contact.email}</a>
         </section>
       </div>
     </>
