c7a198f6f7127eb42c9ea7e72e730c2546903efc
bella <maekawatakaya10.8@gmail.com>
2025-05-08 08:16:42 +0900

    update services routes about Kishax ad.

--- app/routes/services.tsx
+++ app/routes/services.tsx
@@ -28,12 +28,15 @@ export default createRoute(async (c) => {
             <h2 class="text-2xl font-semibold">🎮 Kishax - Minecraft Server</h2>
             <p class="mt-2 text-gray-300">
               Java版と統合版の両方に対応した、小規模で安全なコミュニティサーバーです。
+              <br />
               ガイドや説明文などが英語にも対応していて、日本以外の海外の人でも安心して入れます。
-              独自プラグインやKishaxアカウント連携により、BOT対策やセキュリティも強化しています。
+              <br />
+              独自プラグインやKishaxアカウント連携により、BOT対策やセキュリティも強化したり、
+              プレイヤーが好きなときにサーバーを起動できる環境が整っています。
             </p>
             <ul class="mt-2 list-disc list-inside text-gray-400">
               <li>英語対応でグローバル</li>
-              <li>いつでも自分専用のサーバーを開始可能（サバイバル、MOD、マップ等）</li>
+              <li>いつでも専用のサーバーを起動可能（サバイバル、MOD、マップ等）</li>
               <li>学生や20代中心のフレンドリーなコミュニティ</li>
               <li>統合版プレイヤーも参加可能</li>
               <li>開発者・運営メンバー募集中</li>
