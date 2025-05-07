daadfd4401d6ec5b5836ab63bfe678ec19a569e9
bella <maekawatakaya10.8@gmail.com>
2025-05-07 18:49:01 +0900

    del logger middleware

--- app/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import { MiddlewareHandler } from 'hono'
-
-export const loggerMiddleware: MiddlewareHandler = async (c, next) => {
-  console.log(`[${c.req.method}] ${c.req.url}`)
-  await next()
-}
