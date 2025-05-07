e8036fba38dc237bbab2efb07e5318add35a4afa
bella <maekawatakaya10.8@gmail.com>
2025-05-07 19:13:43 +0900

    do middleware

--- app/routes/[...slug].tsx
+++ app/routes/[...slug].tsx
@@ -2,7 +2,7 @@ import { createRoute } from 'honox/factory';
 
 const oldPostIds = ['8859', '8821', '30026', '34194'];
 
-export default createRoute((c) => {
+export default createRoute(async (c, next) => {
   const path = c.req.path
   const match = path.match(/^\/(\d+)\/?$/)
   const id = match?.[1]
@@ -11,5 +11,5 @@ export default createRoute((c) => {
     return c.redirect(`/blog/posts/${id}/`, 301)
   }
 
-  return c.notFound()
+  await next()
 });
