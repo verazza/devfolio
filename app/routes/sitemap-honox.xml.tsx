0383a97cea48df49164a8c6c74dd2c18918851f0
bella <maekawatakaya10.8@gmail.com>
2025-05-09 00:58:19 +0900

    add sitemap index

---
+++ app/routes/sitemap-honox.xml.tsx
@@ -0,0 +1,55 @@
+import { createRoute } from 'honox/factory';
+import profile from '../../data/profile.json';
+
+const pages = [
+  {
+    loc: '/',
+    lastmod: new Date().toISOString(),
+    priority: '1.0',
+    changefreq: 'daily',
+  },
+  {
+    loc: '/about',
+    lastmod: new Date().toISOString(),
+    priority: '0.7',
+    changefreq: 'daily',
+  },
+  {
+    loc: '/projects',
+    lastmod: new Date().toISOString(),
+    priority: '0.7',
+    changefreq: 'daily',
+  },
+  {
+    loc: '/services',
+    lastmod: new Date().toISOString(),
+    priority: '0.7',
+    changefreq: 'daily',
+  },
+  {
+    loc: '/blog',
+    lastmod: new Date().toISOString(),
+    priority: '0.9',
+    changefreq: 'daily',
+  },
+];
+
+export default createRoute(async (c) => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
+      .map(
+        (page) => `
+    <url>
+      <loc>${profile.link.this}${page.loc}</loc>
+      <lastmod>${page.lastmod}</lastmod>
+      <changefreq>${page.changefreq}</changefreq>
+      <priority>${page.priority}</priority>
+    </url>
+  `
+      )
+      .join('')}
+</urlset>`;
+  c.header('Content-Type', 'application/xml');
+  return c.body(xml);
+});
---
+++ app/routes/sitemap.xml.tsx
@@ -0,0 +1,23 @@
+import { createRoute } from 'honox/factory';
+import profile from '../../data/profile.json';
+
+export default createRoute(async (c) => {
+  const honoxSitemapUrl = `${profile.link.this}/sitemap-honox.xml`;
+  const hexoSitemapUrl = `${profile.link.this}/blog/sitemap.xml`;
+  const lastmod = new Date().toISOString();
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${honoxSitemapUrl}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${hexoSitemapUrl}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  c.header('Content-Type', 'application/xml');
+  return c.body(xml);
+});
